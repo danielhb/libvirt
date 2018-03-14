bb1293cc2396ebc09e9ee6f81ed9a7a0eed02c67
Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
2018-03-14 22:53:15 +0530

    domain: addr: Introduce virDomainPCIAddressEnsureMultifunctionAddress
    Signed-off-by: Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>

--- src/util/virpci.h
+++ src/util/virpci.h
@@ -28,6 +28,8 @@
 # include "virautoclean.h"
 # include "virenum.h"
 
+#define VIR_PCI_MAX_FUNCTIONS 8
+
 typedef struct _virPCIDevice virPCIDevice;
 typedef virPCIDevice *virPCIDevicePtr;
 typedef struct _virPCIDeviceAddress virPCIDeviceAddress;
